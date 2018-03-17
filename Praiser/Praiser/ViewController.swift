95ba361eb63aaa20f8519e1bc9b4b461f12be0fa
icetooth <verbumbox@gmail.com>
2018-03-17 15:30:37 +0300

    init commit

---
+++ Praiser/Praiser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Praiser
+//
+//  Created by Все будет хорошо on 17/03/2018.
+//  Copyright © 2018 verbumbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
